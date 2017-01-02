83dd008f6eb47092a8ef89ae4fa208269edea31f
Reggie Mack <thegraphixninja@gmail.com>
2017-01-02 08:50:27 -0600

    Using ng-model, ng-repeat and ng-click
    Using ng-model, ng-repeat and ng-click

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,19 @@
 angular.module("todoListApp", [])
+
 .controller('mainCtrl', function($scope) {
   $scope.helloWorld = function() {
     console.log("Whatever Mane");
-  };
+};
+
+$scope.todos = [
+    {"name": "clean the house"},
+    {"name": "water the dog"},
+    {"name": "feed the lawn"},
+    {"name": "pay dem bills"},
+    {"name": "run"},
+    {"name": "swim"}
+]
+
 });
     
     
