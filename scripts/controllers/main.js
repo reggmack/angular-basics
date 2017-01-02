c2eaea2eaebfe0095bf17ac1c8a58ac4a7199249
Reggie Mack <thegraphixninja@gmail.com>
2017-01-02 12:02:18 -0600

    Set up Custom Directives
    Set up Custom Directives

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,35 +1,28 @@
 'use strict';
 
 angular.module('todoListApp')
-
 .controller('mainCtrl', function($scope, dataService) {
+  $scope.addTodo = function() {
+    var todo = {name: "This is a new todo."};
+    $scope.todos.unshift(todo);
+  };
   
-    
-   $scope.addTodo = function() {
-     var todo = {name: "This is a new to do."};
-       $scope.todos.push(todo);
-   };
-    
-    dataService.getTodos(function(response) {
-        console.log(response.data);
-        $scope.todos = response.data;
+  $scope.helloWorld = dataService.helloWorld;
+  
+  dataService.getTodos(function(response) { 
+      console.log(response.data);  
+      $scope.todos = response.data;
     });
-    
-    $scope.deleteTodo = function(todo, $index) {
-        dataService.deleteTodo(todo);
-        $scope.todos.splice($index, 1);
-    };
-
-    $scope.saveTodo = function(todo, $index) {
-        dataService.saveTodo(todo);
-    };
-    
-    $scope.todos = [
-        {"name": "clean the house"},
-        {"name": "water the dog"},
-        {"name": "feed the lawn"},
-        {"name": "pay dem bills"},
-        {"name": "run"},
-        {"name": "swim"}
-    ]
+  
+  $scope.deleteTodo = function(todo, $index) {
+    dataService.deleteTodo(todo);
+    $scope.todos.splice($index, 1);
+  };
+  
+  $scope.saveTodo = function(todo) {
+    dataService.saveTodo(todo);
+  };
 })
+
+
+
